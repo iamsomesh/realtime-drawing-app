cae7896958f0f216a8c56aa8157cae129629020c
Somesh Yadav <syb777yadav@gmail.com>
2023-02-01 00:29:23 +0530

    Adding channel draw and mount routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
